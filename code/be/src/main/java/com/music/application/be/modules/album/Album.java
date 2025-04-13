0cf6f0acf16244fee52e0c4cd83c57789595e608
minh10m <minhhh1010@gmail.com>
2025-04-13 21:02:12 +0700

    update module

---
+++ code/be/src/main/java/com/music/application/be/modules/album/Album.java
@@ -0,0 +1,41 @@
+package com.music.application.be.modules.album;
+
+
+import com.music.application.be.modules.artist.Artist;
+import com.music.application.be.modules.song.Song;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "albums")
+@Getter
+@Setter
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name = "cover_image")
+    private String coverImage;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id", nullable = false)
+    private Artist artist;
+
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Song> songs = new ArrayList<>();
+}
