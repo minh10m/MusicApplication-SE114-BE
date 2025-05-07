3c95e533fbde25a748137e7d00ca0c8373710628
Quang Tran <148832427+BunPun231@users.noreply.github.com>
2025-05-08 01:05:25 +0700

    Auth & Swagger config

---
+++ code/be/src/main/java/com/music/application/be/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.music.application.be.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Music Application API")
+                        .version("1.0.0")
+                        .description("API documentation with JWT support"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                ).addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
+
--- code/be/src/main/java/com/music/application/be/config/SecurityConfig.java
+++ code/be/src/main/java/com/music/application/be/config/SecurityConfig.java
@@ -41,11 +41,20 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        req->req.requestMatchers("/login/**","/register/**", "/refresh_token/**")
-                                .permitAll()
+                        req -> req
+                                .requestMatchers(
+                                        "/login/**",
+                                        "/register/**",
+                                        "/refresh_token/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/swagger-resources/**",
+                                        "/configuration/**",
+                                        "/webjars/**"
+                                ).permitAll()
                                 .requestMatchers("/admin/**").hasAuthority("ADMIN")
-                                .anyRequest()
-                                .authenticated()
+                                .anyRequest().authenticated()
                 ).userDetailsService(userDetailsServiceImp)
                 .sessionManagement(session->session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
