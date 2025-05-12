0a52c7d81a7aa1bd33efbdbbb54611130b9e2989
ChuTungDuongg <tungduong156gli@gmail.com>
2025-05-12 15:58:22 +0700

    feat: Them LikedSongScreen va ViewModel cua no, sua api login, sign up

--- app/src/main/java/com/example/musicapplicationse114/model/Album.kt
+++ app/src/main/java/com/example/musicapplicationse114/model/Album.kt
@@ -3,6 +3,6 @@ package com.example.musicapplicationse114.model
 import java.time.LocalDate
 
 data class Album(val id : Long, val name : String,
-                 val releseDate: LocalDate, val coverImage : String,
+                 val releseDate: String, val coverImage : String,
                  val description: String, val artist : Artist, val songs : ArrayList<Song>) {
 }
