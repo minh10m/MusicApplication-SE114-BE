f5b44672f5ae3794258451afe512e96e6370358f
ChuTungDuongg <tungduong156gli@gmail.com>
2025-05-19 22:28:15 +0700

    Them download song

---
+++ app/src/main/java/com/example/musicapplicationse114/ui/screen/download/DownloadSongsViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.musicapplicationse114.ui.screen.download
+
+import androidx.lifecycle.ViewModel
+import com.example.musicapplicationse114.model.Album
+import com.example.musicapplicationse114.model.Artist
+import com.example.musicapplicationse114.model.Genre
+import com.example.musicapplicationse114.model.Song
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class DownloadSongsViewModel : ViewModel() {
+    private val dummyArtist = Artist(
+        id = 1,
+        name = "The Chainsmokers",
+        avatar = "",
+        description = "",
+        followersCount = 0,
+        albums = arrayListOf(),
+        songs = arrayListOf(),
+        followers = arrayListOf()
+    )
+
+    private val dummyAlbum = Album(
+        id = 1,
+        name = "Test Album",
+        releseDate = "2020-01-01",
+        coverImage = "",
+        description = "",
+        artist = dummyArtist,
+        songs = arrayListOf()
+    )
+
+    private val dummyGenre = Genre(
+        id = 1,
+        name = "EDM",
+        description = "",
+        Song = arrayListOf()
+    )
+
+    private val dummySongs = listOf(
+        Song(1, "Inside Out", 220, "", "", "", "2020-01-01", 0, dummyAlbum, dummyArtist, dummyGenre),
+        Song(2, "Young", 210, "", "", "", "2020-01-01", 0, dummyAlbum, dummyArtist, dummyGenre),
+        Song(3, "Beach House", 250, "", "", "", "2020-01-01", 0, dummyAlbum, dummyArtist, dummyGenre),
+        Song(4, "Kills You Slowly", 200, "", "", "", "2020-01-01", 0, dummyAlbum, dummyArtist, dummyGenre)
+    )
+
+    private val _downloadedSongs = MutableStateFlow(dummySongs)
+    val downloadedSongs: StateFlow<List<Song>> = _downloadedSongs
+}
