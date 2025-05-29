b0cb104d9ed736d0ea807378d2951c2d3423dbf9
minh10m <minhhh1010@gmail.com>
2025-05-29 10:14:10 +0700

    update redis first try
    # Conflicts:
    #	code/be/build.gradle
    #	code/be/src/main/java/com/music/application/be/modules/user/UserService.java
    #	code/be/src/main/resources/application.properties

--- code/be/src/main/java/com/music/application/be/modules/user/UserService.java
+++ code/be/src/main/java/com/music/application/be/modules/user/UserService.java
@@ -1,56 +1,42 @@
 package com.music.application.be.modules.user;
 
-import com.music.application.be.modules.cloudinary.CloudinaryService;
 import com.music.application.be.modules.user.dto.UserDTO;
-import jakarta.persistence.EntityNotFoundException;
 import com.music.application.be.modules.user.dto.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final CloudinaryService cloudinaryService;
 
+    @Cacheable(value = "users", key = "#userId")
     public User getUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId));
+        return userRepository.findById(userId).orElseThrow(
+                () -> new RuntimeException("User not found")
+        );
     }
 
-    public User updateUser(Long userId, UserDTO userDTO, MultipartFile avatarFile) throws IOException {
+    @CachePut(value = "users", key = "#userId")
+    public User updateUser(Long userId, UserDTO userDTO) {
         User user = getUserById(userId);
-
-        if (userDTO.getUsername() != null && !userDTO.getUsername().trim().isEmpty()) {
-            user.setUsername(userDTO.getUsername());
-        }
-        if (userDTO.getPhone() != null && !userDTO.getPhone().trim().isEmpty()) {
-            user.setPhone(userDTO.getPhone());
-        }
-        if (userDTO.getEmail() != null && !userDTO.getEmail().trim().isEmpty()) {
-            user.setEmail(userDTO.getEmail());
-        }
-        if (userDTO.getPassword() != null && !userDTO.getPassword().trim().isEmpty()) {
-            user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-        }
-
-        if (avatarFile != null && !avatarFile.isEmpty()) {
-            String avatarUrl = cloudinaryService.uploadFile(avatarFile, "image");
-            user.setAvatar(avatarUrl);
-        }
-
+        user.setUsername(userDTO.getUsername());
+        user.setPhone(userDTO.getPhone());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         return userRepository.save(user);
     }
 
+    @Cacheable(value = "users", key = "'allUsers'")
     public List<UserResponseDTO> getAllUsers() {
         return userRepository.findAll()
                 .stream()
@@ -65,6 +51,7 @@ public class UserService {
                 .toList();
     }
 
+    @CacheEvict(value = "users", key = "#userId")
     public void deleteUser(Long userId) {
         User user = getUserById(userId);
         userRepository.delete(user);
